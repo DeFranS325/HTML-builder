a155e117c948b1c223e47910d1c97b752af86092
DeFranS325 <sansei325@gmail.com>
2022-11-02 13:36:11 +0400

    refactor: change code for function to read from file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,14 @@
-let fs = require('fs');
+const fs = require('fs');
 let path = require('path');
 let dir = `${path.dirname(path.resolve('index.js'))}/01-read-file`;
-fs.readFile(`${dir}/text.txt`, 'utf-8', function (error, fileContent) {
-    if (error) throw error;
-    console.log(fileContent);
+const rs = fs.createReadStream(dir + '/text.txt', 'utf-8');
+rs.on('readable', function () {
+    let data;
+    while ((data = this.read()) != null) {
+        console.log(data);
+    }
+});
+
+rs.on('end', function () {
+    this.close();
 });
---
+++ 02-write-file/index.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+const path = require('path');
+const dir = path.dirname(path.resolve('index.js')) + '/02-write-file';
