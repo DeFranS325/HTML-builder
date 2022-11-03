8f3222fb41dbc48b395551a8694ca7f3edbf7b3e
DeFranS325 <sansei325@gmail.com>
2022-11-03 16:56:54 +0400

    feat: add function according to the 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
+const { stat } = require('fs/promises');
 const path = require('path');
 const secretFolder = path.dirname(path.resolve('./index.js')) + '\\03-files-in-folder\\secret-folder\\';
 
 fs.readdir(secretFolder, { encoding: 'utf8', withFileTypes: true }, (err, files) => {
     if (err) throw err;
+
     for (let file of files) {
         if (!file.isDirectory()) {
             let fileExt = path.extname(file.name);
             let fileName = path.basename(file.name, fileExt);
-            console.log(secretFolder + file.name);
-            let fileSize = fs.stat(secretFolder + file.name, function (err, stats) {
-                stats.size;
-            });            
-            console.log(`${fileName} - ${fileExt.replace('.','')} - ${fileSize}`);
+            fs.stat(secretFolder + file.name, (err, stats) => {
+                if (err) throw err;
+                console.log(`${fileName} - ${fileExt.replace('.', '')} - ${stats.size/1000}kb`);
+            });
         }
     }
 });
